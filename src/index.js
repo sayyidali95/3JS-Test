18f2fc7e2ed1fbbb18eb89425bac357888d53e75
sma69 <sma69@njit.edu>
2018-12-27 13:55:16 -0500

    got gltfLoader working, took my models out of project in prob for different assets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-
+var THREE = window.THREE = require('three');
+require('../js/loaders/GLTFLoader');
 //set up scene
 const scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -8,21 +8,55 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//Add cube
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
 var cube = new THREE.Mesh(geometry, material);
+var clock = new THREE.Clock();
+//var model;
 scene.add(cube);
 
 camera.position.z = 5;
 
+// create a point light
+const pointLight =
+  new THREE.PointLight(0xFFFFFF);
+
+// set its position
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 200;
+pointLight.intensity = 20;
+
+// add to the scene
+scene.add(pointLight);
+
+var loader = new THREE.GLTFLoader();
+
+loader.load( '../models/flameSword2.glb', function ( gltf ) {
+
+    var model = gltf.scene;
+    scene.add(model);
+    model.scale = (.1, .1, .1);
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} );
+
 var animate = function () {
     requestAnimationFrame(animate);
-
+    var scaler = Math.sin(clock.getElapsedTime());
     cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.y += scaler;
+    cube.position.x + scaler
+    cube.scale.x = 1;
+    //model.rotation.x += 0.01;
 
     renderer.render(scene, camera);
 };
+
+
 
 
 animate();
