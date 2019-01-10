09f503d1e8650ef1f3a32e3f9146dafd511208e6
sma69 <sma69@njit.edu>
2019-01-10 01:54:16 -0500

    boundary collision with moving object implmented

--- src/index.js
+++ src/index.js
@@ -10,13 +10,15 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //Add cube
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var boundingBox = new THREE.SphereGeometry(5, 32, 32);
+var geometry = new THREE.SphereGeometry(1, 32, 32);
+var boundingBox = new THREE.BoxGeometry(5, 5, 5);
 var material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
 var wireframeMat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x0000ff});
 var cube = new THREE.Mesh(geometry, material);
 var collisionCube = new THREE.Mesh(boundingBox, wireframeMat);
 var clock = new THREE.Clock();
+var vertex;
+var velocityX, velocityY = 0.0001;
 //var model;
 scene.add(cube);
 scene.add(collisionCube);
@@ -47,16 +49,46 @@ loader.load( '../models/flameSword2.glb', function ( gltf ) {
 
     console.error( error );
 
-} );
+});
 
+velocityX = .008;
+velocityY = .005;
+var velocityZ = .01;
+var boundsRadius = 5;
 var animate = function () {
     requestAnimationFrame(animate);
+    scene.updateMatrixWorld();
     var scaler = Math.sin(clock.getElapsedTime());
-    cube.rotation.x += 0.01;
-    cube.rotation.y += scaler;
-    cube.position.x + scaler
-    cube.scale.x = 1;
-    //model.rotation.x += 0.01;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += scaler;
+    cube.translateX(velocityX);
+    cube.translateY(velocityY);
+    cube.translateZ(velocityZ);
+    var vector = cube.getWorldPosition();
+    //cube.position.x = 0;
+    pointLight.intensity = 20 + 2 * scaler;
+    cube.scale = 1;
+    
+    //make collisions for cube
+    //console.log(vector);
+        if (vector.x + .5 > boundsRadius
+            || -boundsRadius > vector.x - .5) {
+            velocityX *= -1;
+        }
+        if (vector.y + .5 > boundsRadius
+                || -boundsRadius > vector.y - .5) {
+            velocityY *= -1;
+        }
+        if (vector.z + .5 > boundsRadius
+                || -boundsRadius > vector.z - .5) {
+            velocityZ *= -1;
+        }
+        //if (vertex.position.y > collisionCube.innerHeight / 2
+        //   || vertex.position.y > -collisionCube.innerHeight / 2) {
+        //    velocityY *= -1;
+        //}
+    
+   
 
     renderer.render(scene, camera);
 };
